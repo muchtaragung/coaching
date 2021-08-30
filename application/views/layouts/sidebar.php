a51a75dfe103f643cf4411200efdb1ba1766c83a
Rahmat Gibran Zulbadriansyah <rgibran234@gmail.com>
2021-08-30 10:09:41 +0700

    ganti logo di sidebar

--- application/views/layouts/sidebar.php
+++ application/views/layouts/sidebar.php
@@ -2,8 +2,8 @@
 
 	<!-- Sidebar - Brand -->
 	<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-		<div class="sidebar-brand-icon rotate-n-15">
-			<i class="fas fa-laugh-wink"></i>
+		<div class="sidebar-brand-icon ">
+			<i class="fad fa-building"></i>
 		</div>
 		<div class="sidebar-brand-text mx-3">Program Coaching</div>
 	</a>
