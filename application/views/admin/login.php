4affb8fa47a4540f62e909ca5e1aebde9276d53e
Rahmat Gibran Zulbadriansyah <rgibran234@gmail.com>
2021-09-06 11:56:51 +0700

    ubah tulisan di tampilan login admin - header - label - placeholder

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -21,33 +21,27 @@
 							<div class="col-lg-8 mx-auto">
 								<div class="p-5">
 									<div class="text-center">
-										<h1 class="h4 text-gray-900 mb-4">Silahkan Login</h1>
+										<h1 class="h4 text-gray-900 mb-4">Login Admin</h1>
 										<?php if ($this->session->flashdata('login failed')) : ?>
 											<div class="alert alert-danger">
 												<a href="#" class="close" data-dismiss="alert">&times;</a>
 												<?php echo $this->session->flashdata('login failed') ?>
 											</div>
 										<?php endif ?>
-
-										<?php
-										if (isset($_SESSION['login failed'])) {
-											unset($_SESSION['login failed']);
-										}
-										?>
 									</div>
 									<form action="<?php echo site_url('admin/auth') ?>" method="post">
 										<div class="form-group">
-											<label for="username">username</label>
-											<input class="form-control" type="text" name="username" id="username" placeholder="masukan email">
+											<label for="username">Username Admin</label>
+											<input class="form-control" type="text" name="username" id="username" placeholder="Email Admin">
 										</div>
 										<div class="form-group">
-											<label for="password">Password</label>
-											<input class="form-control" type="password" name="password" id="password" placeholder="masukan password">
+											<label for="password">Password Admin</label>
+											<input class="form-control" type="password" name="password" id="password" placeholder="Password Admin">
 										</div>
-										<input class="form-control btn btn-primary" type="submit" value="masuk">
+										<input class="form-control btn btn-primary" type="submit" value="Logins">
 									</form>
 									<hr>
-									
+
 								</div>
 							</div>
 						</div>
