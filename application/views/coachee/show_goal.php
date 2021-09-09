44f3bfe327a6d5721fbd5bd19829a2766caf0f86
rgibranz <rgibran234@gmail.com>
2021-09-09 14:20:47 +0700

    fix tulisan tambah peserta di tambah action plan

--- application/views/coachee/show_goal.php
+++ application/views/coachee/show_goal.php
@@ -290,7 +290,7 @@
 			<div class="modal-dialog" role="document">
 				<div class="modal-content">
 					<div class="modal-header">
-						<h5 class="modal-title" id="exampleModalLabel">Tambah Peserta</h5>
+						<h5 class="modal-title" id="exampleModalLabel">Tambah Action Plan</h5>
 						<button class="close" type="button" data-dismiss="modal" aria-label="Close">
 							<span aria-hidden="true">×</span>
 						</button>
