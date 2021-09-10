9bbdccd78db1af76a622d6b917ce1dad0f94a41e
rgibranz <rgibran234@gmail.com>
2021-09-10 14:39:00 +0700

    fix hitung tanggal sisa hari due date goal

--- application/views/coach/coachee/goal.php
+++ application/views/coach/coachee/goal.php
@@ -40,9 +40,9 @@
 						$hasil = $diff;
 						?>
 						<?php if ($hasil < 0) : ?>
-							<span class="badge badge-pill badge-secondary"><?= "Sisa Due Date  " . abs(ceil($hasil / (60 * 60 * 24))) . ' Hari' ?></span>
+							<span class="badge badge-pill badge-secondary"><?= "Sisa Due Date  " . abs(ceil($hasil / (60 * 60 * 24)) - 1) . ' Hari' ?></span>
 						<?php else : ?>
-							<span class="badge badge-pill badge-danger"><?= "Goal Terlewat " . ceil($hasil / (60 * 60 * 24)) . ' Hari'; ?></span>
+							<span class="badge badge-pill badge-danger"><?= "Goal Terlewat " . ceil($hasil / (60 * 60 * 24) + 1) . ' Hari'; ?></span>
 						<?php endif ?>
 						<br>
 						<?php if ($goal->status == 'selesai') : ?>
